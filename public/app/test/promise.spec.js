bcdd21b9c5b9f67801af60900b3dc97e560b16cc
Anthony Bonifacio Alvarez <Anthony Bonifacio Alvarez>
2016-06-12 00:08:36 +0200

    format code in public directory

--- public/app/test/promise.spec.js
+++ public/app/test/promise.spec.js
@@ -22,45 +22,47 @@ describe('Testing a Controller that uses a Promise', function () {
         module('demoApp.services');
     });
         
-        beforeEach(inject(function($controller, _$rootScope_, _$q_, dataService) {
-          $q = _$q_;
-          $scope = _$rootScope_.$new();
-          // We use the $q service to create a mock instance of defer
-          deferred = _$q_.defer();
-          
-          // Use a Jasmine Spy to return the deferred promise
-          spyOn(dataService, 'getData').and.returnValue(deferred.promise);
-          
-          // Init the controller, passing our spy service instance
-          vm = $controller('MainCtrl', { 
-            $scope: $scope, 
-            dataService: dataService
-          });
-        }));
-        it('should resolve promise', function () {
-          // Setup the data we wish to return for the .then function in the controller
-          deferred.resolve(dataTest);          
-          vm.loadDataService();
-          
-          // We have to call apply for this to work
-          $scope.$apply();
-          
-          // Since we called apply, not we can perform our assertions
-          expect(vm.json).not.toBe(undefined);
-          expect(vm.json.length).toBe(3);
-          expect(vm.error).toBe(undefined);          
-        });
+    beforeEach(inject(function($controller, _$rootScope_, _$q_, dataService) {
+        $q = _$q_;
+        $scope = _$rootScope_.$new();
+        // We use the $q service to create a mock instance of defer
+        deferred = _$q_.defer();
         
-        it('should reject promise', function () {
-          // This will call the .catch function in the controller
-          deferred.reject();
-          vm.loadDataService();
-          
-          // We have to call apply for this to work
-          $scope.$apply();
-          
-          // Since we called apply, not we can perform our assertions
-          expect(vm.json).toBe(undefined);
-          expect(vm.error).toBe('There has been an error!');
+        // Use a Jasmine Spy to return the deferred promise
+        spyOn(dataService, 'getData').and.returnValue(deferred.promise);
+        
+        // Init the controller, passing our spy service instance
+        vm = $controller('MainCtrl', { 
+          $scope: $scope, 
+          dataService: dataService
         });
+    }));
+    
+    it('should resolve promise', function () {
+        // Setup the data we wish to return for the .then function in the controller
+        deferred.resolve(dataTest);          
+        vm.loadDataService();
+        
+        // We have to call apply for this to work
+        $scope.$apply();
+        
+        // Since we called apply, not we can perform our assertions
+        expect(vm.json).not.toBe(undefined);
+        expect(vm.json.length).toBe(3);
+        expect(vm.error).toBe(undefined);          
+    });
+      
+    it('should reject promise', function () {
+        // This will call the .catch function in the controller
+        deferred.reject();
+        vm.loadDataService();
+        
+        // We have to call apply for this to work
+        $scope.$apply();
+        
+        // Since we called apply, not we can perform our assertions
+        expect(vm.json).toBe(undefined);
+        expect(vm.error).toBe('There has been an error!');
       });
+
+});
